5f658353a5a9d5eb21c308d0db490e28d0ed0241
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-06-30 10:22:21 +0100

    [chore]: integrate treblle sdk

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'treblle',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'treblle.middleware.TreblleMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -121,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TREBLLE_INFO = {
+'api_key': config('TREBLLE_API_KEY'),
+'project_id': config('TREBLLE_PROJECT_ID'),
+}
